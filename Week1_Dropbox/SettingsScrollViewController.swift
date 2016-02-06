cf718feafaa340099e656fef948d27657fbaae51
Jenn Leung <jleung@jleung.local>
2016-02-06 01:36:20 -0800

    added existing user

---
+++ Week1_Dropbox/SettingsScrollViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SettingsScrollViewController.swift
+//  Week1_Dropbox
+//
+//  Created by Jenn Leung on 2/6/16.
+//  Copyright © 2016 Jenn Leung. All rights reserved.
+//
+
+import UIKit
+
+class SettingsScrollViewController: UIViewController {
+
+    @IBOutlet weak var existingUserSettingsScrollView: UIScrollView!
+    @IBOutlet weak var settingsScrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
